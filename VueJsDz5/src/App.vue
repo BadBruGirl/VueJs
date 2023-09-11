ef341dfb7867045179f16a0715800dfdfe4c539f
Kseniya <kukinaksu@yandex.ru>
2023-09-11 12:40:37 +0300

    Add folder whith dz5

---
+++ VueJsDz5/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ProductDateils />
+  </div>
+</template>
+
+<script>
+import ProductDateils from './components/ProductDateils.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDateils
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueJsDz5/src/components/ProductDateils.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h3>{{ product.name }}</h3>
+        <p>Стоимость: {{ formattedPrice }}</p>
+        <P class="green" v-if="product.balance">Остаток: {{ product.balance }}</P> 
+        <p class="red" v-else>{{ 'Товара нет в наличии!' }}</p>
+        <p>Описание: {{ product.diskription}}</p>
+        <hr>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ProductDateils',
+    data() {
+        return {
+            currency: '₽',
+            product: {
+                id: 1, 
+                name: 'Товар 1', 
+                price: 1999.45, 
+                balance: 0, 
+                diskription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, perferendis.'
+            }
+        };
+    },
+
+    computed: {
+        formattedPrice(){
+            return  this.currency + ' ' + this.product.price;
+        }
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.red{
+    color: red;
+}
+.green{
+    color: green;
+}
+
+</style>
